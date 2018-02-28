9c9be3c6e1077322068c267848528220878a0733
Gsowmya111 <sowmyakarnati1112gmail.com>
2018-02-28 11:30:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sowmyaram/emailsending_background/Config.java
@@ -0,0 +1,9 @@
+package com.example.sowmyaram.emailsending_background;
+
+/**
+ * Created by sowmyaram on 12/26/2017.
+ */
+public class Config {
+    public static final String EMAIL ="sowmya2025@gmail.com / from email id";
+    public static final String PASSWORD ="provide password";
+}
---
+++ app/src/main/java/com/example/sowmyaram/emailsending_background/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.sowmyaram.emailsending_background;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Send button
+    private Button buttonSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      buttonSend = (Button) findViewById(R.id.buttonSend);
+
+        //Adding click listener
+        buttonSend.setOnClickListener(this);
+    }
+
+
+    private void sendEmail() {
+        //Getting content for email
+      /*  String email = editTextEmail.getText().toString().trim();
+        String subject = editTextSubject.getText().toString().trim();
+        String message = editTextMessage.getText().toString().trim();
+*/
+
+        String email = "provide sender or To email id  /  ";
+
+        String subject = "hi";
+        String message = "hello";
+
+        //Creating SendMail object
+        SendMail sm = new SendMail(this, email, subject, message);
+
+        //Executing sendmail to send email
+        sm.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        sendEmail();
+    }
+}
